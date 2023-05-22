67ca7157ae789ac6138e8fd47bb49415884c34c8
Patrick <patrickmoreno444@gmail.com>
2023-05-21 17:25:19 -0700

    master feat: add creating todos on specific day

--- src/components/Day.vue
+++ src/components/Day.vue
@@ -1,21 +1,33 @@
 <template>
     <div class="page-day">
+      <template v-if="!isCreating">
         <h1>{{ dayName }}</h1>
         <div 
             v-if="todos && todos.length"
             class="todos-container"
         >
-            <input type="checkbox" :checked="todo.checked"/>
             <div 
-                v-for="(todo, index) in todos"
-                :key="index"
+              v-for="(todo, index) in todos"
+              :key="index"
             >
-                {{ todo.name }}
+              <input type="checkbox" :checked="todo.checked"/>
+                {{ todo.task }}
             </div>
         </div>
         <div v-else class="todos-container">
             What to do {{ dayName.toLowerCase() }}...
         </div>
+        <div class="add-todo-btn">
+          <span class="add-font" @click="openCreate">+</span>
+        </div>
+      </template>
+      <template v-else>
+        <div class="close-create-todo-btn" @click="isCreating = false">Close</div>
+        <div class="create-todo-input-box">
+          Due {{ dayName }}
+          <input type="text" v-model="newTodo" class="create-todo-input" @keyup.enter="createTodo" :ref="`create-todo-${dayName.toLowerCase()}`"/>
+        </div>
+      </template>
     </div>
 </template>
 
@@ -35,24 +47,67 @@ export default {
     }
   },
   data () {
+    // get localstorage and set here?
+
     return {
+      list: [],
+      isCreating: false,
+      newTodo: ''
     }
   },
   computed: {
     todos () {
-      return this.items
+      const filteredByDay = this.list.filter(t => t.day === this.dayName.toLowerCase())
+      return [...this.items, ...filteredByDay]
     }
   },
   methods: {
     createTodo () {
-      //save to locaStorage as the day
+      //TODO save to locaStorage
+      this.list.push({
+        task: this.newTodo,
+        checked: false,
+        day: this.dayName.toLowerCase()
+      })
+      this.newTodo = ''
+      this.isCreating = false
+    },
+    openCreate () {
+      this.isCreating = true
+      this.$nextTick(() => {
+        this.$refs[`create-todo-${this.dayName.toLowerCase()}`].focus()
+      })
     }
   }
 }
 </script>
 
 <style>
-    .todos-container {
-        margin-left: 1.5rem;
-    }
+  .close-create-todo-btn {
+    margin: 1rem;
+  }
+  .create-todo-input {
+    border: none;
+    border-bottom: 2px solid #000;
+    border-left: 2px solid #000;
+    border-right: 2px solid #000;
+  }
+  .create-todo-input-box {
+    display: flex;
+    align-items: center;
+    flex-direction: column;  
+  }
+  .todos-container {
+      margin-left: 1.5rem;
+  }
+  .add-font {
+    font-size:36pt;
+  }
+  .add-todo-btn {
+    position: absolute;
+    bottom:5%;
+    display: flex;
+    justify-content: center;
+    width:100%;
+  }
 </style>
