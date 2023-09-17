5de1137cedfd4103cc6f35546b155b2a4b15dacc
patrickm4 <patrickmoreno444@gmail.com>
2023-09-17 14:36:15 -0700

    vue3-migration feat: upgrade vue 2 to vue 3

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
+// import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import './style.css'
 
-Vue.config.productionTip = false
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+import { createApp } from 'vue'
+
+const app = createApp(App)
+app.mount('#app')
